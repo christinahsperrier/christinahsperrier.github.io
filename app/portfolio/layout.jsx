f25a20f001171ef2f1260db855052af0677d13b7
christinahsperrier <143152255+christinahsperrier@users.noreply.github.com>
2026-02-10 23:54:55 -0500

    added portfolio page

--- app/portfolio/layout.jsx
+++ app/portfolio/layout.jsx
@@ -1,5 +1,6 @@
 export default function PortfolioLayout({ children }){
     return <>
+        <h1 className="display-4 color-1 text-center text-md-start mb-4">Portfolio</h1>
         {children}
     </>
 }
