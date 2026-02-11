1931a46e169fc7e8d7431d2851fba7bfcc87bcae
christinahsperrier <143152255+christinahsperrier@users.noreply.github.com>
2026-02-10 23:49:21 -0500

    moved social to writing page

--- app/portfolio/writing/page.jsx
+++ app/portfolio/writing/page.jsx
@@ -2,6 +2,35 @@
 import config from '../../config.json'
 import { text_font } from '../../fonts'
 
+const social_media = (
+    title,
+    type,
+    image,
+    media,
+    link
+) => {
+    return <>
+        <div className={`masonry-item ${text_font.className}`}>
+            <div className="card overflow-hidden masonry-content">
+                <img src={`/multimedia/${image}`} alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title fw-bold">{title}</h5>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h6 className="card-subtitle m-0 color-1">{type}</h6>
+                        <a href={link ? link : `/multimedia/${media}`} className="d-flex align-items-center view-story ms-2" target="_blank" rel="noopener noreferrer">
+                            <span className="me-1">View media</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
 const card = (
     title,
     type,
@@ -38,6 +67,11 @@ export default function Writing(){
         <h6 className={`mb-3 fw-normal ${text_font.className}`}>{config.stories.editorial_subheader}</h6>
         <div className="portfolio-grid">
             {config.stories.editorial.map(story => card(story.title, story.type, story.image, story.link))}
+        </div>
+        <h2 className="color-1 mt-3 mb-1 fw-bold">Social Media</h2>
+        <h6 className={`mb-3 fw-normal ${text_font.className}`}>Selected Work</h6>
+        <div className="portfolio-grid">
+            {config.social_media.map(social => social_media(social.title, social.type, social.image, social.media, social.link))}
         </div>
         <h2 className="color-1 my-3 mt-4 fw-bold">Academic</h2>
         <div className="portfolio-grid">
