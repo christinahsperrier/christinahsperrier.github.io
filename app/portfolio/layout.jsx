c112c2a3453ae90917ed5a480867975d7a4abdac
christinahsperrier <143152255+christinahsperrier@users.noreply.github.com>
2026-02-11 00:49:17 -0500

    removed header from layout

--- app/portfolio/layout.jsx
+++ app/portfolio/layout.jsx
@@ -1,6 +1,5 @@
 export default function PortfolioLayout({ children }){
     return <>
-        <h1 className="display-4 color-1 text-center text-md-start mb-4">Portfolio</h1>
         {children}
     </>
 }
